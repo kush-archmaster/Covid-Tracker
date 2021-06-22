131963aaf5f41c6fb60fb5c0bee8bde3a0a56300
Kushagra Upadhyay <updkush08@gmail.com>
2021-06-22 13:18:52 +0530

    covid-tracker app

--- src/index.js
+++ src/index.js
@@ -8,3 +8,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+//# Covid-Tracker
+//It is a react application which fetches real-time data from API regarding situation of Covid-19 cases all over India
